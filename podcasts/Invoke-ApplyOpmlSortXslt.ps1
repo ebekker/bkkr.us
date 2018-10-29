2b5309af420c755ccf5de22d0da7d594bd982b4a
Eugene Bekker <ebekker@users.noreply.github.com>
2018-10-29 14:29:28 -0400

    updating with parameter values

--- podcasts/Invoke-ApplyOpmlSortXslt.ps1
+++ podcasts/Invoke-ApplyOpmlSortXslt.ps1
@@ -2,4 +2,23 @@
 
 $xsl = [System.Xml.Xsl.XslCompiledTransform]::new()
 $xsl.Load("$PWD\opml-sort.xsl")
-$xsl.Transform("$PWD\podcasts_opml.xml", "$PWD\podcasts_opml-sorted.xml")
+
+$prm = [System.Xml.Xsl.XsltArgumentList]::new()
+
+## Transform feed of podcasts
+$prm.Clear()
+$prm.AddParam('feedType', '', 'Podcast')
+$prm.AddParam('notesFile', '', 'podcast-notes.xml')
+$prm.AddParam('listSelect', '', 'outline2')
+$out = [System.IO.StreamWriter]::new("$PWD\podcasts_opml-sorted.xml", $false)
+$xsl.Transform("$PWD\podcasts_opml.xml", $prm, $out)
+$out.Dispose()
+
+## Transform feed of news
+$prm.Clear()
+$prm.AddParam('feedType', '', 'NewsFeed')
+$prm.AddParam('notesFile', '', 'greader-notes.xml')
+$prm.AddParam('listSelect', '', 'outline1')
+$out = [System.IO.StreamWriter]::new("$PWD\greader_opml-sorted.xml", $false)
+$xsl.Transform("$PWD\greader_opml.xml", $prm, $out)
+$out.Dispose()
